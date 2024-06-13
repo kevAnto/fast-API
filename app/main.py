36aac9fe6a9d4c3a38a0cd440b5667d9a0127dd3
Antoine CHOULA <76167680+kevAnto@users.noreply.github.com>
2024-06-13 18:18:05 +0100

    New Update main.py

--- app/main.py
+++ app/main.py
@@ -4,10 +4,9 @@ app = FastAPI()
 
 @app.get("/")
 async def read_main():
-<<<<<<< HEAD
+
     return {"msg": "Thank You Patrick"}
-=======
-    return {"msg": "THANK YOU Gauthier"}
->>>>>>> fe65e9be4dd531fe8ce8d1eb3d4693aa6735122f
+
+
 
 
